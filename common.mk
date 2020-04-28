3c3c87f7b7219b1a395095b8cb3d5b4a8971a6ae
Anthony Stange <stange@google.com>
2020-04-28 12:03:18 -0400

    sm8250-common: Update sensors multihal to 2.1
    Bug: 149758467
    Test: Verify that the new unit tests and subhals in this topic load and
    run on a Pixel device
    
    Change-Id: I275274659816b3a00374fb0b7a93f199874afdcd

--- common.mk
+++ common.mk
@@ -373,7 +373,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal
+    android.hardware.sensors@2.1-service.multihal
 
 PRODUCT_PACKAGES += \
     libsensorndkbridge \
