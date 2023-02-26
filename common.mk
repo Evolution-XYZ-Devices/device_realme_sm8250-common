6e0058d2b0005b9e8dcc6facbdc3cb3753eb599d
someone5678 <nemui3353@gmail.com>
2023-02-26 19:16:42 +0530

    sm8250-common: Create dummy libldacBT_bco
    * This target is never existed and Bluetooth apex module also doesn't actually use it.
      But module still opening the library. Which cause log spam.
    
    * This commit is not required if you can just edit the source.
    
    Signed-off-by: IGaganpreetSingh <gaganpannu83@gmail.com>

--- common.mk
+++ common.mk
@@ -102,7 +102,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
+    vendor.qti.hardware.btconfigstore@2.0.vendor \
+    libldacBT_bco
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
